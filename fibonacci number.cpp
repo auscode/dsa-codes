7abadbb94d978ea82f4ab8711a521b33259c81dc
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2022-12-15 08:36:31 +0530

    fib

--- fibonacci number.cpp
+++ fibonacci number.cpp
@@ -1,13 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    int fib(int n) {
-        if (n==0){
+    int fib(int n)
+    {
+        if (n == 0)
+        {
             return 0;
         }
-        if (n<=2){
+        if (n <= 2)
+        {
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        return fib(n - 1) + fib(n - 2);
     }
-
 };
