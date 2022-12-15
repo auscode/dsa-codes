9d51c87fe7735d935167fa992ba7a0026867a45e
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2022-12-15 08:54:38 +0530

    342. Power of Four

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    bool isPowerOfFour(int n)
+    {
+        if (n == 0)
+        {
+            return false;
+        }
+        else if (n == 1 || n == 4)
+        {
+            return true;
+        }
+        else if (n % 4 != 0)
+        {
+            return false;
+        }
+        return isPowerOfFour(n / 4);
+    }
+};
