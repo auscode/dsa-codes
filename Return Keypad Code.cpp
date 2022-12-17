242a32921b14c0a84a5230e3b2ef375c8b4b0b3d
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2022-12-17 08:02:45 +0530

    Return Keypad Code

--- Return Keypad Code.cpp
+++ Return Keypad Code.cpp
@@ -2,44 +2,49 @@
 #include <string>
 using namespace std;
 
-int keypad(int num, string output[],string options[]){
-    if (num==0){
-        output[0]="";
+int keypad(int num, string output[], string options[])
+{
+    if (num == 0)
+    {
+        output[0] = "";
         return 1;
     }
-    int lastDigit =num%10;
-    int smallNumber = num/10;
+    int lastDigit = num % 10;
+    int smallNumber = num / 10;
     string smallOutput[500];
 
-    int smallCount =keypad(smallNumber,smallOutput,options);
+    int smallCount = keypad(smallNumber, smallOutput, options);
     string op = options[lastDigit];
 
-    int k =0;
-    for (int i=0; i<op.length();i++){
-        for (int j=0;j<smallCount;j++){
-            output[k] = smallOutput[j]+op[i];
+    int k = 0;
+    for (int i = 0; i < op.length(); i++)
+    {
+        for (int j = 0; j < smallCount; j++)
+        {
+            output[k] = smallOutput[j] + op[i];
             k++;
         }
     }
-return k;
+    return k;
 }
 
-int keypad(int num, string output[]){
+int keypad(int num, string output[])
+{
     /* Insert all the possible combinations of the integer number into the output string array. You do not need to
     print anything, just return the number of strings inserted into the array.
     */
-    string options[]= {"","", "abc", "def","ghi", "jkl","mno", "pqrs","tuv","wxyz" };
-    return keypad(num,output,options);
-    
-
+    string options[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    return keypad(num, output, options);
 }
-int main(){
+int main()
+{
     int num;
     cin >> num;
 
     string output[10000];
     int count = keypad(num, output);
-    for(int i = 0; i < count && i < 10000; i++){
+    for (int i = 0; i < count && i < 10000; i++)
+    {
         cout << output[i] << endl;
     }
     return 0;
