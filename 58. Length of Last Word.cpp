a8a1ae88552b7894f805320067149ada7fa5f119
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2022-12-17 07:24:14 +0530

    58. Length of Last Word

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -1,14 +1,19 @@
 // 58. Length of Last Word in a string
+#include <bits/stdc++.h>
+
 class Solution
 {
 public:
-    int lengthOfLastWord(string s)
+    int lengthOfLastWord(std::string s)
     {
         int len = 0, tail = s.length() - 1;
+        // if the string contains any space at the last it goes into this block
         while (tail >= 0 && s[tail] == ' ')
         {
             tail--;
         }
+
+        // otherwise it will go into this block
         while (tail >= 0 && s[tail] != ' ')
         {
             len++;
