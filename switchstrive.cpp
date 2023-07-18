3650444622292b18d6ddc34ecd62f56abbad86cb
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2023-07-18 12:26:42 +0530

    prob4

---
+++ switchstrive.cpp
@@ -0,0 +1,16 @@
+double areaSwitchCase(int ch, vector<double> a)
+{
+    // Write your code here
+    switch (ch)
+    {
+    case 1:
+        return M_PI * a[0] * a[0];
+        break;
+    case 2:
+        return a[0] * a[1];
+        break;
+    default:
+        return -1;
+    }
+    return 0;
+}
