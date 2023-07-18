f48c97f5c280c746c377d17bd128ab8c8ce462b0
Harshit Mishra <76650831+auscode@users.noreply.github.com>
2023-07-18 12:12:29 +0530

    prob3

---
+++ ifelse.cpp
@@ -0,0 +1,16 @@
+string compareIfElse(int a, int b)
+{
+    // Write your code here
+    if (a < b)
+    {
+        return "smaller";
+    }
+    else if (a > b)
+    {
+        return "greater";
+    }
+    else
+    {
+        return "equal";
+    }
+}
